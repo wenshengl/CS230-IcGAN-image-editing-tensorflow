ef10b64b1930218e6ced6a42f578459c0cc28d93
孙铂尧 <boysun@ucdavis.edu>
2018-05-18 16:35:22 -0700

    resize data

--- load_data.py
+++ load_data.py
@@ -3,20 +3,29 @@ import errno
 import numpy as np
 import scipy
 import scipy.misc
+from scipy.misc import imresize
+from scipy.misc import imsave
 import matplotlib.pyplot as plt
 
 
 data_dir = os.path.join("./data", "mini_img/")
 filenames = os.listdir(data_dir)
 
-X = []
-for imge_file in filenames:
-    x = plt.imread(data_dir + imge_file)
-    # x = imresize()
-    X.append(x)
+# X = []
+i = 0
+for image_file in filenames:
+	if image_file.endswith('.png'):
+	    x = plt.imread(data_dir + image_file)
+	    x = imresize(x, (28, 28))
+	    #X.append(x)
+	    imsave('resize_' + str(i) + '.png', x)
+	    i += 1
 
-X = np.array(X)
+# X = np.array(X)
 
-np.save(data_dir + 'X', X)
-print (X.shape)
+# np.save(data_dir + 'X', X)
+# X = np.load(data_dir + 'X.npy')
+#print (X.shape)
+# plt.imshow(X[29])
+# plt.show()
 
