f2389d5cadc5b7a3d0fe5d7be5ba350369fb6628
Vincentli8540 <36247164+Vincentli8540@users.noreply.github.com>
2018-05-19 11:39:38 -0700

    new API of main

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import datetime
 from  utils import mkdir_p
 from utils import MnistData, celebA
 from Gan import Gan
+from Gan_celebA import Gan_celebA
 import numpy as np
 
 flags = tf.app.flags
@@ -16,6 +17,7 @@ flags.DEFINE_integer("celebA", 1, 'Choose celebA dataset')
 
 FLAGS = flags.FLAGS
 
+
 if __name__ == "__main__":
 
     root_log_dir = "/tmp/logs/mnist_test"
@@ -25,6 +27,8 @@ if __name__ == "__main__":
     sample_path = "sample/mnist_gan"
 
     OPER_FLAG = FLAGS.OPER_FLAG
+
+
 
     if OPER_FLAG == 0:
 
@@ -45,11 +49,11 @@ if __name__ == "__main__":
     now = datetime.datetime.now(dateutil.tz.tzlocal())
     timestamp = now.strftime('%Y_%m_%d_%H_%M_%S')
 
-    batch_size = 64
+    # batch_size = 1
     max_epoch = 20
 
     #for mnist train 62 + 2 + 10
-    sample_size = 64
+    # sample_size = 512
     dis_learn_rate = 0.0002
     gen_learn_rate = 0.0002
 
@@ -64,17 +68,30 @@ if __name__ == "__main__":
     mkdir_p(encode_z_checkpoint_dir)
     mkdir_p(encode_y_checkpoint_dir)
 
-    data, label = celebA().load_celebA()
-    print (data.shape)
 
-    celebA = FLAGS.celebA
 
-    if celebA == 0:
+    celebA_FLAG = FLAGS.celebA
+
+    if celebA_FLAG == 0:
+        batch_size = 64
+        sample_size = 64
         print ('Training MNIST dataset:')
         data , label = MnistData().load_mnist()
+        infoGan = Gan(batch_size=batch_size, max_epoch=max_epoch, build_model_flag = build_model_flag,
+                      model_path=root_checkpoint_dir, encode_z_model=encode_z_checkpoint_dir,encode_y_model=encode_y_checkpoint_dir,
+                      data=data,label=label, extend_value=FLAGS.extend,
+                      network_type="mnist", sample_size=sample_size,
+                      sample_path=sample_path , log_dir = log_dir , gen_learning_rate=gen_learn_rate, dis_learning_rate=dis_learn_rate , info_reg_coeff=1.0)
 
 
-    infoGan = Gan(batch_size=batch_size, max_epoch=max_epoch, build_model_flag = build_model_flag,
+    else: 
+        batch_size = 1
+        sample_size = 512
+        print("cnm")
+        data, label = celebA().load_celebA()
+        print('nmb')
+        print (data.shape)
+        infoGan = Gan_celebA(batch_size=batch_size, max_epoch=max_epoch, build_model_flag = build_model_flag,
                       model_path=root_checkpoint_dir, encode_z_model=encode_z_checkpoint_dir,encode_y_model=encode_y_checkpoint_dir,
                       data=data,label=label, extend_value=FLAGS.extend,
                       network_type="mnist", sample_size=sample_size,
@@ -84,22 +101,22 @@ if __name__ == "__main__":
 
     if OPER_FLAG == 0:
 
-        print "Training Gan"
+        print ("Training Gan")
         infoGan.train()
 
     elif OPER_FLAG == 1:
 
-        print "Training encode for z"
+        print ("Training encode for z")
         infoGan.train_ez()
 
     elif OPER_FLAG == 2:
 
-        print "Training encode for Y"
+        print ("Training encode for Y")
         infoGan.train_ey()
 
     else:
 
-        print "This is Test"
+        print ("This is Test")
         infoGan.test()
 
 
